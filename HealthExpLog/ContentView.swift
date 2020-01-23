2835edf730aec29b5e269489358289473cedc123
Qiang Ma <qiangma7@gmail.com>
2020-01-23 14:26:23 -0500

    Initial Commit

---
+++ HealthExpLog/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HealthExpLog
+//
+//  Created by ARMSTRONG on 1/23/20.
+//  Copyright © 2020 ARMSTRONG. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
